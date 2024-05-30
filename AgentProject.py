662be2d84c7a287c9ed7aa41d34873e5a59f5585
kirthika85 <kirthika.rajamani@gmail.com>
2024-05-30 07:14:02 -0700

    Update AgentProject.py

--- AgentProject.py
+++ AgentProject.py
@@ -115,7 +115,7 @@ class VectorStoreRetriever:
     @classmethod
     def from_docs(cls, docs, oai_client):
         embeddings = oai_client.embeddings.create(model="text-embedding-ada-002", input=[doc["page_content"] for doc in docs])
-        print("Embeddings:", embeddings)
+        #print("Embeddings:", embeddings)
         #vectors = [emb["embedding"] for emb in embeddings["data"]]
         vectors = [emb.embedding for emb in embeddings.data]
         print("Vectors:", vectors)
